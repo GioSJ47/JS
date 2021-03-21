9543b9cc03972125b5224a878cfcaa657b24e74b
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-03-21 01:23:52 -0300

    Create detectarTeclaPulsada.js

---
+++ Librerias/detectarTeclaPulsada.js
@@ -0,0 +1,51 @@
+/*
+INFO:
+	AUTOR: GioSJ
+	GITHUB: https://github.com/GioSJ47
+	VERSION: 1.1
+	FECHA: 21/03/2021 (DD/MM/AAAA)
+	DESCRIPCION: ESTE SCRIPT PERMITE MODIFICAR STRINGS A TU NECESIDAD.
+  
+FORMA DE USO:
+  tecla: almacena la última tecla precionada y permite el funcionamiento de "teclado.accion".
+  
+  teclas: es un array que va almacenando las teclas precionadas en orden.
+  
+  accion: este objeto se usa para almacenar la funcion que se quiere ejecutar cuando se precionen teclas.
+  Ejemplo 1 de uso:
+    teclado.accion=function(){
+      console.log("Tecla precionada: " + teclado.tecla);
+    };
+  Ejemplo 1 de uso:
+    
+  iniciar(): esta función inicia el funcionamiento de "teclado". Una vez tenga configurado el funcionamiento
+  dentro de "accion",
+  teclaPulsada(): esta función indica si alguna tecla ya fué pulsada devolviendo true o false.
+  Ejemplos de uso:
+    teclado.teclaPulsada("a");
+    teclado.teclaPulsada("Enter");
+    teclado.teclaPulsada("Shift");
+   
+   reiniciar(): esta función vacía el array "teclas" (historial de pulsaciones), pero no vacía el objeto "accion".
+*/
+
+var teclado = {
+    tecla: false,
+    teclas: new Array(),
+    accion: false,
+  
+    iniciar: function() {
+        document.onkeydown = teclado._accion;
+    },
+    _accion: function(e){
+        teclado.tecla=e.key;  
+        teclado.teclas.push(teclado.tecla);
+        if(teclado.accion)teclado.accion();
+    },
+    teclaPulsada: function(codigoTecla){
+        return (teclado.teclas.indexOf(codigoTecla) !== -1) ? true : false;
+    },
+    reiniciar: function() {
+        teclado.teclas = new Array();
+    }
+};
