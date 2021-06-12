67d43403c3131011eb1a7905b37d88e9a4395d03
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-06-12 01:12:06 -0300

    Update scrollFunciones.js

--- Mecanicas/scrollFunciones.js
+++ Mecanicas/scrollFunciones.js
@@ -1,3 +1,10 @@
+/*
+DEFINICION DE SCROLL:
+  Se denomina Scroll (desplazamiento 2D) al desplazamiento de arriba a abajo (vertical) o de
+  lado a lado (horizontal) de los contenidos que forman una página web, una app, etc.
+*/
+
+
 //Para obtener el tamaño interno de un elemento con scroll
   function scrollHeight(e){
     return e.scrollHeight - e.clientHeight;
@@ -6,7 +13,7 @@
 
 
 
-//Para obtener en la posicion donde se encuentra el scroll
+//Para obtener la posicion donde se encuentra el scroll
   function scrollY(e){
     return e.scrollHeight*(e.scrollTop/(e.scrollTop+e.clientHeight))
   }
@@ -14,10 +21,26 @@
 
 
 
-//Para dirigirse a una posicion del scroll
+//Para dirigir el scroll DIRECTAMENTE a una posicion
   document.getElementById("miElemento").scrollTop = 150; //150px
+
+
+
+//Para dirigir el scroll CON UN EFECTO DE SUAVIDAD a una posicion
+  document.getElementById("miElemento").scroll({ top: 150, behavior: 'smooth' });
 
 
 
 //Para avanzar desde la posicion actual del scroll
   document.getElementById("miElemento").scrollTop = scrollY(document.getElementById("miElemento")) + 150; //Posicion actual + 150px
+
+
+
+//Detectar cuando el usuario usa un scroll
+  document.getElementById("miElemento").addEventListener('scroll', function(e) {
+    //EL CODIGO QUE SE TIENE QUE EJECUTAR CUANDO SE USE UN SCROLL
+  }
+
+
+
+//
