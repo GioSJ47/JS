2c1944567092f1493dbc34917e1496e1ec4fb681
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-06-12 00:38:57 -0300

    Update scrollFunciones.js

--- Mecanicas/scrollFunciones.js
+++ Mecanicas/scrollFunciones.js
@@ -1,11 +1,23 @@
 //Para obtener el tamaño interno de un elemento con scroll
-  document.getElementById("miElemento").scrollHeight;
+  function scrollHeight(e){
+    return e.scrollHeight - e.clientHeight;
+  }
+  //EJ: scrollHeight(document.getElementById("miElemento"));
+
+
 
 //Para obtener en la posicion donde se encuentra el scroll
   function scrollY(e){
-      return e.scrollTop*(e.scrollHeight/(e.scrollHeight-e.clientHeight))
+    return e.scrollHeight*(e.scrollTop/(e.scrollTop+e.clientHeight))
   }
   //EJ: scrollY(document.getElementById("miElemento"));
 
+
+
 //Para dirigirse a una posicion del scroll
-  
+  document.getElementById("miElemento").scrollTop = 150; //150px
+
+
+
+//Para avanzar desde la posicion actual del scroll
+  document.getElementById("miElemento").scrollTop = scrollY(document.getElementById("miElemento")) + 150; //Posicion actual + 150px
