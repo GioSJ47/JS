b5dfa0f28b33e3d7ff52fb0608c1d8cc01fffe56
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-06-15 19:32:26 -0300

    Update predefinidos.js

--- Funciones/Arrays/predefinidos.js
+++ Funciones/Arrays/predefinidos.js
@@ -1,1 +1,45 @@
+//DECLARACIONES DE ARRAYS VACIOS
+ var arr = Array()
+ var arr = new Array()
+ var arr = []
  
+//DECLARACIONES DE ARRAYS CON CIERTO ESPACIO (CAPACIDAD MAXIMA)
+ var arr = Array(10)
+ var arr = new Array(10)
+
+//DECLARACIONES DE ARRAYS CON VALORES INICIALES
+ var arr = Array(1, 2, 3, 4)
+ var arr = new Array(1, 2, 3, 4)
+ var arr = [ 1, 2, 3, 4 ]
+
+//VER CUANTOS ELEMENTOS ALMACENA UN ARRAY
+ var arr = [ 1, 2, 3, 4 ]
+ arr.length // 4
+
+//AGREGAR UN VALOR AL PRINCIPIO
+ var arr = [ 1, 2, 3, 4 ]
+ arr.unshift(0) // [ 0, 1, 2, 3, 4 ]
+
+//AGREGAR VALOR AL FINAL
+ var arr = [ 1, 2, 3, 4 ]
+ arr.push(5)         // [ 1, 2, 3, 4, 5 ]
+ arr[arr.length] = 6 // [ 1, 2, 3, 4, 5, 6 ]
+
+//ELIMINAR EL PRIMER ELEMENTO
+ var arr = [ 1, 2, 3, 4 ]
+ arr.shift(); // [ 1, 2, 3 ]
+
+//ELIMINAR EL ULTIMO ELEMENTO
+ var arr = [ 1, 2, 3, 4 ]
+ arr.pop() // [ 1, 2, 3 ]
+
+ 
+//RECONOCER SI SE TRATA DE UN ARRAY/OBJETO
+ let arr = [ 1, 2, 3, 4 ]; 
+ typeof arr == "object" //true
+
+//RECONOCER SI SE TRATA DE UN ARRAY
+ let arr = [ 1, 2, 3, 4 ]; 
+ Array.isArray(arr) //true
+
+//
