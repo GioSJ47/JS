0db7dc15b3e5e402e4e2b37f22ad7f7855549e13
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-06-30 21:58:44 -0300

    Update trigonometria.js

--- Librerias/matematica/trigonometria.js
+++ Librerias/matematica/trigonometria.js
@@ -1,63 +1,61 @@
 // H O A G
 function trig (datos, formato, out=false){
-	
-	formato = formato.toUpperCase().split("");
+	if(datos.length==2&&(formato.length==3||(formato.length==2&&out.length==1))){
+		formato = formato.toUpperCase().split("");
 
-	let h = (arr, buscar)=>{ return arr.indexOf(buscar)+1 }
-	let m = (buscar)=>{ return datos[formato.indexOf(buscar)] }
-	
-	let x;
-	if(out){
-		for(i in datos){
-			if(!datos[i])x=formato[i]
+		let h = (arr, buscar)=>{ return arr.indexOf(buscar)+1 }
+		let m = (buscar)=>{ return datos[formato.indexOf(buscar)] }
+
+		let x;
+		if(out){
+			for(i in datos){
+				if(!datos[i])x=formato[i]
+			}
+		} else {
+			x=out;
 		}
-	} else {
-		x=out;
-	}
-  
-	//SENO O COSENO
-  if(h(formato, "H") || h(out, "H")){
-    //SENO
-		if(h(formato, "O") || h(out, "O")){
-      if(x=="H"){
-				return m("O") / Math.sin(m("G"))
-			}
-			if(x=="O"){
-				return m("H") * Math.sin(m("G"))
-			}
-			if(x=="G"){
-				return Math.asin(m("O") / m("H"))
-			}
-    } else
 
-		//COSENO
-		if(h(formato, "A") || h(out, "A")){
-			if(x=="H"){
-				return m("A") / Math.cos(m("G"))
-			}
+		//SENO O COSENO
+		if(h(formato, "H") || h(out, "H")){
+			//SENO
+			if(h(formato, "O") || h(out, "O")){
+				if(x=="H"){
+					return m("O") / Math.sin(m("G"))
+				}
+				if(x=="O"){
+					return m("H") * Math.sin(m("G"))
+				}
+				if(x=="G"){
+					return Math.asin(m("O") / m("H"))
+				}
+			} else
+
+			//COSENO
+			if(h(formato, "A") || h(out, "A")){
+				if(x=="H"){
+					return m("A") / Math.cos(m("G"))
+				}
+				if(x=="A"){
+					return m("H") * Math.cos(m("G"))
+				}
+				if(x=="G"){
+					return Math.acos(m("A") / m("H"))
+				}
+			}		
+		} else
+
+		//TANGENTE
+		if((h(formato, "A")||h(out, "A")) && (h(formato, "O")||h(out, "O"))){
 			if(x=="A"){
-				return m("H") * Math.cos(m("G"))
-			}
-			if(x=="G"){
-				return Math.acos(m("A") / m("H"))
-			}
-		}		
-  } else
-
-	//TANGENTE
-	if((h(formato, "A")||h(out, "A")) && (h(formato, "O")||h(out, "O"))){
-    if(x=="A"){
-				return m("O") / Math.tan(m("G"))
-			}
-			if(x=="O"){
-				return m("A") * Math.tan(m("G"))
-			}
-			if(x=="G"){
-				return Math.atan(m("O") / m("A"))
-			}
-  }
-
-	else {
-		return false;
+					return m("O") / Math.tan(m("G"))
+				}
+				if(x=="O"){
+					return m("A") * Math.tan(m("G"))
+				}
+				if(x=="G"){
+					return Math.atan(m("O") / m("A"))
+				}
+		}
 	}
+	return false;
 }
