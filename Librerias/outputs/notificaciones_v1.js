88332b1100a047e10c3ea415624295c8428acb0a
GioSJ47 <43457511+GioSJ47@users.noreply.github.com>
2021-07-01 10:54:44 -0300

    Create notificaciones_v1.js

---
+++ Librerias/outputs/notificaciones_v1.js
@@ -0,0 +1,53 @@
+/*
+    Script creado el 01/07/2021 (dd/mm/aaaa) por GioSJ
+    Código en GitHub: 
+*/
+
+///////////////////////////
+// FUNCIONES PRINCIPALES //
+///////////////////////////
+
+  //FUNCION PARA ENVIAR NOTIFICACIONES CON AUTOLIMPIEZA (AUTODESTRUCCION)
+  function notifiTemporal(msg, titulo="", icono="", t=5000) {
+    var opciones = {
+        body: msg,
+        icon: icono
+    }
+    if(notiPermiso()) var n = new Notification(titulo,opciones);
+    setTimeout(n.close.bind(n), t);
+  }
+  
+  //FUNCION PARA EMITIR NOTIFICACION
+  function notifi(msg, titulo="", icono=""){
+    var opciones = {
+        body: msg,
+        icon: icono
+    }
+    if(notiPermiso()) var n = new Notification(titulo, opciones);
+  }
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCIONES QUE PERMITEN EL CORRECTO FUNCIONAMIENTO DE LAS FUNCIONES ANTERIORMENE DECLARADAS //
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //FUNCION PARA PEDIR PERMISO DE NOTIFICACION AL USUARIO
+  function notiPermiso(){
+    if (window.Notification && Notification.permission !== "granted"){
+      Notification.requestPermission(function (status) {
+        if (Notification.permission !== status)
+          Notification.permission = status;
+          return true;
+      });
+      return false;
+    } else return true;
+  }
+  
+  //FUNCION PARA VER SI LA PAGINA YA TIENE PERSMISO DE NOTIFICAR
+  function notiSiPermiso(){
+    if (window.Notification && Notification.permission === "granted") return true;
+    else return false;
+  }
